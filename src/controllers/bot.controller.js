2baac69a735fb32c1943f6faf084a5d0f78a5a12
sathiyendren <sathiyendren@gmail.com>
2021-11-11 00:09:16 +0530

    bot udpated

--- src/controllers/bot.controller.js
+++ src/controllers/bot.controller.js
@@ -18,21 +18,3 @@ const updateBot = catchAsync(async (req, res) => {
 module.exports = {
   updateBot,
 };
-
-
-logger.info(`req.body.pair: ${req.body.pair}`);
-logger.info(`req.body.isReadyToBuy: ${req.body.isReadyToBuy}`);
-logger.info(`req.body.isReadyToSell: ${req.body.isReadyToSell}`);
-
-let pair = '';
-if (req.body.pair) {
-  const pairArray = req.body.pair.split('USDT');
-  pair = `USDT_${pairArray[0]}`;
-}
-const bot = await botService.getBotByPair(pair);
-bot.isReadyToBuy = req.body.isReadyToBuy;
-
-bot.isReadyToBuy = (isReadyToBuy === 1) ? true : false
-const updatedBot = await botService.updateBot(bot.id, bot);
-res.send(updatedBot);
-});
