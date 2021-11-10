6ffdca0e39749658a065c2012e44a452fe45a87d
sathiyendren <sathiyendren@gmail.com>
2021-11-11 00:08:50 +0530

    bot udpated

--- src/controllers/bot.controller.js
+++ src/controllers/bot.controller.js
@@ -5,23 +5,34 @@ const logger = require('../config/logger');
 const updateBot = catchAsync(async (req, res) => {
   logger.info(`req.body.pair: ${req.body.pair}`);
   logger.info(`req.body.isReadyToBuy: ${req.body.isReadyToBuy}`);
-  logger.info(`req.body.isReadyToSell: ${req.body.isReadyToSell}`);
-
   let pair = '';
   if (req.body.pair) {
     const pairArray = req.body.pair.split('USDT');
     pair = `USDT_${pairArray[0]}`;
   }
   const bot = await botService.getBotByPair(pair);
-  bot.isReadyToBuy = req.body.isReadyToBuy;
-  const botParams = {
-    pair,
-    isReadyToBuy: isReadyToBuy === 1 ? true : false,
-  };
-  const updatedBot = await botService.updateBot(bot.id, botParams);
+  const updatedBot = await botService.updateBot(bot.id, req.body);
   res.send(updatedBot);
 });
 
 module.exports = {
   updateBot,
 };
+
+
+logger.info(`req.body.pair: ${req.body.pair}`);
+logger.info(`req.body.isReadyToBuy: ${req.body.isReadyToBuy}`);
+logger.info(`req.body.isReadyToSell: ${req.body.isReadyToSell}`);
+
+let pair = '';
+if (req.body.pair) {
+  const pairArray = req.body.pair.split('USDT');
+  pair = `USDT_${pairArray[0]}`;
+}
+const bot = await botService.getBotByPair(pair);
+bot.isReadyToBuy = req.body.isReadyToBuy;
+
+bot.isReadyToBuy = (isReadyToBuy === 1) ? true : false
+const updatedBot = await botService.updateBot(bot.id, bot);
+res.send(updatedBot);
+});
